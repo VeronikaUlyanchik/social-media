f6fb8ed221ab411142d79a14c982d3d71f019c44
Veronika Ulyanchik <veronikaullll1998@gmail.com>
2022-03-09 00:09:07 +0300

    55 way

--- my-app/src/components/Users/UsersContainer.ts
+++ my-app/src/components/Users/UsersContainer.ts
@@ -1,22 +1,30 @@
 import { connect } from 'react-redux';
 import React from 'react';
 import {AppStateType, reducersType} from '../../redux/reduxState';
-import { followAC , setUsersAC, unfollowAC, UsersStateType, UserType } from '../../redux/users-reducer';
+import { followAC , setCurrentPageAC, setTotalUsersCountAC, setUsersAC, unfollowAC, UsersStateType, UserType } from '../../redux/users-reducer';
 import {Dispatch} from 'redux';
 import { UsersC } from './UsersС';
 
 type MapStateToPropsType  = {
     users: Array<UserType>
+    currentPage: number
+    numberOnPage: number
+    usersCount: number
 }
 export type mapDispatchToPropsType  = {
     follow: (userId: number) => void
     unfollow: (userId: number) => void
     setUsers: (state: Array<UserType>) => void
+    setCurrentPage: (currentPage: number) => void
+    setTotalUsersCount: (totalUsers: number) => void
 }
 
 const mapStateToProps=(state: AppStateType): MapStateToPropsType=> {
     return {
-        users: state.usersPage.users
+        users: state.usersPage.users,
+        currentPage: state.usersPage.currentPage,
+        numberOnPage: state.usersPage.numberOnPage,
+        usersCount: state.usersPage.usersCount
     }
 }
 const mapDispatchToProps=(dispatch: Dispatch): mapDispatchToPropsType=> {
@@ -29,7 +37,13 @@ const mapDispatchToProps=(dispatch: Dispatch): mapDispatchToPropsType=> {
         },
         setUsers: (state: Array<UserType>) => {
             dispatch(setUsersAC(state))
-        }
+        },
+        setCurrentPage: (currentPage: number) => {
+            dispatch(setCurrentPageAC(currentPage))
+        },
+        setTotalUsersCount: (totalUsers: number) => {
+            dispatch(setTotalUsersCountAC(totalUsers))
+        },
     }
 }
 
--- my-app/src/redux/users-reducer.ts
+++ my-app/src/redux/users-reducer.ts
@@ -3,15 +3,10 @@ import { dispatchActionType} from "./state";
 const FOLLOW = 'FOLLOW';
 const UNFOLLOW = 'UNFOLLOW';
 const SET_USERS = 'SET_USERS';
+const SET_CURRENT_PAGE = 'SET_CURRENT_PAGE';
+const SET_TOTAL_USERS_COUNT= 'SET_TOTAL_USERS_COUNT';
 
-// export type UserType = {
-//     id: number
-//     photo: string
-//     fullname: string
-//     status: string
-//     followed: boolean,
-//     location: { county: string, city: string }
-// }
+
 export type UserType = {
     name: string
     id: number
@@ -23,9 +18,15 @@ export type UserType = {
 
 export type UsersStateType = {
     users: Array<UserType>
+    currentPage: number
+    numberOnPage: number
+    usersCount: number
 }
 let initialState:UsersStateType = {
-    users: []
+    users: [],
+    currentPage: 1,
+    numberOnPage:5,
+    usersCount:0,
 };
 
 export const usersReducer = (state: UsersStateType = initialState, action: UserActionType): UsersStateType=> {
@@ -37,19 +38,29 @@ export const usersReducer = (state: UsersStateType = initialState, action: UserA
             return {...state, users: state.users.map(u=> u.id === action.userId ? {...u, followed: true} : u)};
         }
         case SET_USERS: {
-            return {...state, users: [...state.users, ...action.state]};
+            return {...state, users: action.state};
+        }
+        case SET_CURRENT_PAGE: {
+            return {...state, currentPage: action.currentPage};
+        }
+        case SET_TOTAL_USERS_COUNT: {
+            return {...state, usersCount: action.totalUsers};
         }
         default:
             return state
     }
 }
 
-export type UserActionType = FollowActionType | UnFollowActionType | setUsersActionType;
+export type UserActionType = FollowActionType | UnFollowActionType | setUsersActionType | setCurrentPageType | setTotalUsersCountType ;
 
 type FollowActionType = ReturnType<typeof followAC>
 type UnFollowActionType = ReturnType<typeof unfollowAC>
 type setUsersActionType = ReturnType<typeof setUsersAC>
+type setCurrentPageType = ReturnType<typeof setCurrentPageAC>
+type setTotalUsersCountType = ReturnType<typeof setTotalUsersCountAC>
 
 export const followAC = (userId: number) => ({type: FOLLOW, userId}) as const;
 export const unfollowAC = (userId: number) => ({type: UNFOLLOW, userId}) as const;
 export const setUsersAC = (state:Array<UserType>) => ({type: SET_USERS, state}) as const;
+export const setCurrentPageAC = (currentPage:number) => ({type: SET_CURRENT_PAGE, currentPage}) as const;
+export const setTotalUsersCountAC = (totalUsers:number) => ({type: SET_TOTAL_USERS_COUNT, totalUsers}) as const;
