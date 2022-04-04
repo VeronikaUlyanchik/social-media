e0cf6716716f358d1f7e348150f17a63a5d2795b
Veronika Ulyanchik <veronikaullll1998@gmail.com>
2022-04-04 19:24:47 +0300

    75way

--- my-app/src/components/Login/Login.tsx
+++ my-app/src/components/Login/Login.tsx
@@ -1,5 +1,35 @@
+import {Field, Form, Formik} from 'formik';
 import React from 'react';
 
 export const Login = () => {
-    return <h1>Login</h1>
+    return (
+        <div>
+            <h1>Login</h1>
+            <LoginForm/>
+        </div>
+    )
+}
+
+const LoginForm = () => {
+    return (
+        <Formik
+            initialValues={{login: '', password: '', rememberMe: false}}
+            onSubmit={(values, {setSubmitting}) => {
+                console.log(values)
+                setTimeout(() => {
+                    setSubmitting(false);
+                }, 400);
+            }}>
+            {({isSubmitting}) => (
+                <Form>
+                    <div><Field placeholder="Login" name="login"/></div>
+                    <div><Field type="password" name="password" placeholder="Password"/></div>
+                    <div><Field type="checkbox" name="rememberMe"/> remember me</div>
+                    <button type="submit"  disabled={isSubmitting}>
+                        Submit
+                    </button>
+                </Form>
+            )}
+        </Formik>
+    )
 }
